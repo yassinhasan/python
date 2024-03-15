b3d15952f087290c6180f3ceaa0f62359df7ec8b
yassinhasan <figo781@gmail.com>
2024-03-15 13:39:40 +0300

    refresh

--- static/js/script.js
+++ static/js/script.js
@@ -27,3 +27,9 @@ logo.addEventListener("click",()=>{
     window.location.href="/"
 })
 
+let menu_btn = document.querySelector(".menu-btn");
+let inner_links = document.querySelector(".inner-links");
+menu_btn.addEventListener("click",()=>{
+    inner_links.classList.toggle("show")
+})
+
