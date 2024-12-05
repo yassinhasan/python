08b8c427986d853cdc978b9342bd57e487200b01
root <root@hasan>
2024-12-05 17:00:29 +0300

    repair pushing gitignore files 2

---
+++ server/src/config .example.py
@@ -0,0 +1,42 @@
+config = {
+  "type": "service_account",
+  "project_id": "drnull-f3805",
+  "private_key_id": "",
+  "private_key": "",
+  "client_email": "firebase-adminsdk-4yvab@drnull-f3805.iam.gserviceaccount.com",
+  "client_id": "109398182894280224948",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "",
+  "universe_domain": "googleapis.com" ,
+  "apiKey": "",
+  "authDomain": "drnull-f3805.firebaseapp.com",
+  "projectId": "drnull-f3805",
+  "storageBucket": "",
+  "messagingSenderId": "",
+  "appId": "1",
+  "measurementId": "",
+  'databaseURL': ''
+}
+
+mail_config = {
+    
+"MAIL_SERVER":"smtp.gmail.com",
+"MAIL_PORT" : 465,
+"MAIL_USERNAME" : "",
+"MAIL_PASSWORD" : "",
+"MAIL_USE_TLS" : True,
+"MAIL_USE_SSL" : True,
+"APP_NAME"    : "drnull"
+}
+
+
+# MAIL_MAILER=smtp
+# MAIL_HOST=smtp.gmail.com
+# MAIL_PORT=465
+# MAIL_USERNAME=figo781@gmail.com
+# MAIL_PASSWORD=riequjgaldjxscmh
+# MAIL_ENCRYPTION=tls
+# MAIL_FROM_ADDRESS=hasan781@gmail.com
+# MAIL_FROM_NAME="${APP_NAME}"
